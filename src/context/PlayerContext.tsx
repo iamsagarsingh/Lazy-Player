12d72a9993b3478e6418184716dac4d46f9473d2
sagar singh <sagar.17jccs049@jietjodhpur.ac.in>
2025-06-22 16:09:53 +0530

    fix useReducer decalaration.

--- src/context/PlayerContext.tsx
+++ src/context/PlayerContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useReducer } from "react";
+import { createContext, useContext } from "react";
 import type { Song } from "../types";
 import { useLocalStorage } from "../hooks/useLocalStorage";
 
