ed5f248e14ddb58a626abfa026c9fb8a43f9e3f4
sagar singh <sagar.17jccs049@jietjodhpur.ac.in>
2025-06-22 16:17:48 +0530

    fixing navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,10 +2,10 @@ import logo from '../assets/logo.png'
 export const Navbar = () => {
     return <nav className='flex items-center rounded-b-sm shadow'>
         <div className='flex items-center w-3/5 md:w-1/3'>
-            <img src={logo} alt="logo-navbar" className='h-[6rem]'/>
+            <img src={logo} alt="logo-navbar" className='h-[4rem] md:h-[6rem]'/>
             <h2 className='font-extrabold text-xl md:text-2xl text-blue-400'>Lazy Player</h2>
         </div>
-        <ul className='flex gap-2 md:gap-10 md:text-xl font-bold'>
+        <ul className='flex gap-2 md:gap-10 md:text-xl font-bold px-1'>
             <li>Music</li>
             <li>Songs</li>
             <li>Metals</li>
