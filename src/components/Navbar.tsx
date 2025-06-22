0561263ee824977fd2c10ca3ebe066ba908c3075
sagar singh <sagar.17jccs049@jietjodhpur.ac.in>
2025-06-22 16:15:05 +0530

    update logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ export const Navbar = () => {
     return <nav className='flex items-center rounded-b-sm shadow'>
         <div className='flex items-center w-3/5 md:w-1/3'>
             <img src={logo} alt="logo-navbar" className='h-[6rem]'/>
-            <h2 className='font-extrabold text-2xl text-blue-400'>Lazy Player</h2>
+            <h2 className='font-extrabold text-xl md:text-2xl text-blue-400'>Lazy Player</h2>
         </div>
         <ul className='flex gap-2 md:gap-10 md:text-xl font-bold'>
             <li>Music</li>
